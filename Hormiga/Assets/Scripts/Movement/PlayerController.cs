5593a0cc7ee4160ea346fe68fce69c8f65e2dc54
ShadowShot07 <javier.vera.suarez380@gmail.com>
2024-07-03 14:47:17 +0100

    Añadido Player y quitado fricción
    Se ha añadido un script para los valores de Player, cambiado el controller para que no tenga fricción con las paredes y sabe si esta en el suelo

--- Hormiga/Assets/Scripts/Movement/PlayerController.cs
+++ Hormiga/Assets/Scripts/Movement/PlayerController.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     private PlayerInput _playerInput;
+
     [SerializeField] private Vector2 _playerDirection;
     [SerializeField] private float _playerSpeed;
+    [SerializeField] private Transform _groundController;
+    [SerializeField] private LayerMask _groundMask;
+    [SerializeField] private Vector3 _boxDimension;
 
-    private Rigidbody2D _playerRB;
+    private Player _player;
+
+    private bool _grounded;
+
     private InputAction _moveAction;
     private InputAction _interactionAction;
+
+    private bool _lookRight = true;
+    private Rigidbody2D _playerRB;
 
     private void Awake()
     {
@@ -25,6 +32,8 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         _playerDirection = _moveAction.ReadValue<Vector2>();
+
+        _grounded = Physics2D.OverlapBox(_groundController.position, _boxDimension, 0f, _groundMask);
     }
 
     private void FixedUpdate()
@@ -34,6 +43,12 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        _playerRB.velocity = new Vector2(_playerSpeed * _playerDirection.x, _playerRB.velocity.y);
+        if (_grounded) { _playerRB.velocity = new Vector2(_playerSpeed * _playerDirection.x, _playerRB.velocity.y); }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(_groundController.position, _boxDimension);
     }
 }
---
+++ Hormiga/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private string _playerName;
+    [SerializeField] private Sprite _playerSprite;
+}
+
