68c7715676abf365332c734b22e166b094c37a95
ShadowShot07 <javier.vera.suarez380@gmail.com>
2024-07-04 17:21:33 +0100

    Nombre de escena en reloj y prefab del reloj
    Añadido que añada en el centro el nombre de la escena y hecho el prefab del reloj

--- Hormiga/Assets/Scripts/TimeManager/TimeManager.cs
+++ Hormiga/Assets/Scripts/TimeManager/TimeManager.cs
@@ -1,37 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TimeManager : MonoBehaviour
 {
-    [SerializeField] RectTransform _clockArrow;
-    [SerializeField] int _firstHourAngle;      
-    [SerializeField] int _secondHourAngle;      
-    [SerializeField] int _thirstHourAngle;      
-    [SerializeField] int _fourthHourAngle;      
+    [SerializeField] private RectTransform _clockArrow;
+    [SerializeField] private int _firstHourAngle;      
+    [SerializeField] private int _secondHourAngle;      
+    [SerializeField] private int _thirstHourAngle;      
+    [SerializeField] private int _fourthHourAngle;
+    [SerializeField] private TextMeshProUGUI dayText;
 
-    private string FirthHour = TimeConstants.MORNING;
+    private string FirsthHour = TimeConstants.MORNING;
     private string SecondHour = TimeConstants.AFTERNOON;
     private string ThirthHour = TimeConstants.DUSK;
     private string FourthHour = TimeConstants.NIGHT;
 
+    private void Awake()
+    {
+        dayText.text = SceneManager.GetSceneByName("RelojPrueba").name;
+    }
+
     void Start()
     {
-        
+        ClockArrowMove(FirsthHour, _firstHourAngle);  
     }
 
-    // Update is called once per frame
     void Update()
     {
-        ClockArrowMove();
+        //ClockChange();
     }
 
-    private void ClockArrowMove()
+    private void ClockArrowMove(string hour, int angle)
     {
-        if (FirthHour == TimeConstants.MORNING) { _clockArrow.rotation = Quaternion.Euler(0, 0, _firstHourAngle); }
-        if (SecondHour == TimeConstants.AFTERNOON) { _clockArrow.rotation = Quaternion.Euler(0, 0, _secondHourAngle); }
-        if (ThirthHour == TimeConstants.DUSK) { _clockArrow.rotation = Quaternion.Euler(0, 0, _thirstHourAngle); }
-        if (FourthHour == TimeConstants.NIGHT) { _clockArrow.rotation = Quaternion.Euler(0, 0, _fourthHourAngle); }
+        if (hour == TimeConstants.MORNING) { _clockArrow.rotation = Quaternion.Euler(0, 0, angle); }
+        if (hour == TimeConstants.AFTERNOON) { _clockArrow.rotation = Quaternion.Euler(0, 0, angle); }
+        if (hour == TimeConstants.DUSK) { _clockArrow.rotation = Quaternion.Euler(0, 0, angle); }
+        if (hour == TimeConstants.NIGHT) { _clockArrow.rotation = Quaternion.Euler(0, 0, angle); }
+    }
+
+    private void ClockChange()
+    {
+        //if (mission complete) { ClockArrowMove(SecondHour, _secondHourAngle); }
+        //if (mission complete) { ClockArrowMove(ThirthHour, _thirstHourAngle); }
+        //if (mission complete) { ClockArrowMove(FourthHour, _fourthHourAngle); }
     }
 }
